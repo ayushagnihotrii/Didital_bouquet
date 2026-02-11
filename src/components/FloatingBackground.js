ad1a92c444b656f9ac7c7ed5fc59d4b9224fc617
kakashi-hatake7 <omg.uno.reverse@gmail.com>
2026-02-11 12:20:00 +0530

    Increase floating background flower size & opacity for better desktop visibility

--- src/components/FloatingBackground.js
+++ src/components/FloatingBackground.js
@@ -5,26 +5,26 @@ import FlowerSVG from './FlowerSVG';
 
 const backgroundFlowers = [
   // Top-left cluster
-  { id: 'rose', x: 6, y: 8, size: 70, rotate: -20, delay: 0, duration: 5 },
-  { id: 'lavender', x: 2, y: 28, size: 55, rotate: 15, delay: 1.2, duration: 6.5 },
-  { id: 'daisy', x: 12, y: 52, size: 50, rotate: -35, delay: 0.6, duration: 5.5 },
+  { id: 'rose', x: 4, y: 5, size: 110, rotate: -20, delay: 0, duration: 5 },
+  { id: 'lavender', x: 1, y: 30, size: 90, rotate: 15, delay: 1.2, duration: 6.5 },
+  { id: 'daisy', x: 8, y: 55, size: 85, rotate: -35, delay: 0.6, duration: 5.5 },
 
   // Top-right cluster
-  { id: 'orchid', x: 88, y: 5, size: 65, rotate: 25, delay: 0.8, duration: 6 },
-  { id: 'tulip', x: 92, y: 35, size: 55, rotate: -10, delay: 1.5, duration: 5.8 },
+  { id: 'orchid', x: 86, y: 3, size: 100, rotate: 25, delay: 0.8, duration: 6 },
+  { id: 'tulip', x: 90, y: 32, size: 88, rotate: -10, delay: 1.5, duration: 5.8 },
 
   // Bottom-left
-  { id: 'peony', x: 3, y: 72, size: 60, rotate: 30, delay: 0.3, duration: 6.2 },
-  { id: 'anemone', x: 10, y: 90, size: 48, rotate: -45, delay: 1.8, duration: 5.2 },
+  { id: 'peony', x: 2, y: 72, size: 95, rotate: 30, delay: 0.3, duration: 6.2 },
+  { id: 'anemone', x: 7, y: 90, size: 80, rotate: -45, delay: 1.8, duration: 5.2 },
 
   // Bottom-right
-  { id: 'sunflower', x: 90, y: 65, size: 68, rotate: 10, delay: 0.5, duration: 5.5 },
-  { id: 'chrysanthemum', x: 85, y: 88, size: 52, rotate: -20, delay: 1.0, duration: 6.8 },
+  { id: 'sunflower', x: 88, y: 62, size: 105, rotate: 10, delay: 0.5, duration: 5.5 },
+  { id: 'chrysanthemum', x: 84, y: 85, size: 85, rotate: -20, delay: 1.0, duration: 6.8 },
 
   // Scattered mid-edges
-  { id: 'iris', x: 5, y: 45, size: 45, rotate: 40, delay: 2.0, duration: 7 },
-  { id: 'carnation', x: 94, y: 52, size: 48, rotate: -30, delay: 1.4, duration: 6 },
-  { id: 'lily', x: 15, y: 15, size: 42, rotate: 55, delay: 2.2, duration: 5.3 },
+  { id: 'iris', x: 3, y: 45, size: 78, rotate: 40, delay: 2.0, duration: 7 },
+  { id: 'carnation', x: 92, y: 50, size: 82, rotate: -30, delay: 1.4, duration: 6 },
+  { id: 'lily', x: 12, y: 14, size: 72, rotate: 55, delay: 2.2, duration: 5.3 },
 ];
 
 export default function FloatingBackground() {
@@ -45,7 +45,7 @@ export default function FloatingBackground() {
           style={{
             left: `${flower.x}%`,
             top: `${flower.y}%`,
-            opacity: 0.09,
+            opacity: 0.18,
             transform: `rotate(${flower.rotate}deg)`,
             animation: `bgFloat ${flower.duration}s ease-in-out ${flower.delay}s infinite`,
           }}
